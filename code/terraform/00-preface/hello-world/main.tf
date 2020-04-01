f876984ea78c9f5593e2329bc41e671e4b6a04cb
Rizart Dokollari <r.dokollari@gmail.com>
2020-04-01 11:39:29 +0100

    Switch to ireland ami.

--- code/terraform/00-preface/hello-world/main.tf
+++ code/terraform/00-preface/hello-world/main.tf
@@ -3,14 +3,14 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region = "eu-west-1"
 
   # Allow any 2.x version of the AWS provider
   version = "~> 2.0"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+resource "aws_instance" "micro" {
+  ami           = "ami-04d5cc9b88f9d1d39"
   instance_type = "t2.micro"
 }
 
